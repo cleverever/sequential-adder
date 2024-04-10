4bac029a588b4cbbc81be03d6cfeb56f841eec4a
cleverever <60687194+cleverever@users.noreply.github.com>
2024-04-09 19:42:17 -0700

    Bug fixes

--- sequential_adder.sv
+++ sequential_adder.sv
@@ -12,20 +12,20 @@ module sequential_adder #(parameter SIZE = 8)
 );
 
 logic [SIZE:0] temp0;
-logic [SIZE-1:0] temp1;
+logic [SIZE:0] temp1;
 
 always_comb begin
-    {carry, sum} = temp0;
-    done = ~(|temp1);
+    {carry, sum} = temp1;
+    done = ~(|temp0);
 end
 
 always_ff @(posedge clk) begin
     if(start) begin
-        temp0 <= a;
-        temp1 <= b;
+        temp0 <= {1'b0, a};
+        temp1 <= {1'b0, b};
     end
     else begin
-        temp0 <= {(temp0[SIZE-1:0] & temp1), 1'b1};
+        temp0 <= {(temp0[SIZE-1:0] & temp1), 1'b0};
         temp1 <= temp0 ^ temp1;
     end
 end
